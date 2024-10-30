a78ced0929e688274249a0abb3acf1111e262140
Francisco Javier <francisco.rodriguez.garcia.al@iespoligonosur.org>
2024-10-30 08:49:48 +0100

    marina

--- ITV/urls.py
+++ ITV/urls.py
@@ -4,4 +4,9 @@ from .import views
 
 urlpatterns = [
     path('', views.index,name="urls_index"),
-]
+    path('clientes/listar_clientes', views.listar_clientes,name="urls_listar_clientes"),
+    path('citas/cita_cliente/<int:id_cliente>', views.cita_cliente,name="urls_cita_cliente"),
+    path('estaciones/local_precio', views.estaciones_con_locales,name="urls_estaciones_con_locales"),
+    path('trabajadores/trabajador_estacion/<int:id_estacion>', views.trabajadores_estacion,name="urls_trabajadores_estacion"),
+    path('inspecciones/<str:matricula>', views.inspecciones_vehiculo,name="urls_inspecciones_vehiculo"),
+]   
--- ITV/views.py
+++ ITV/views.py
@@ -1,25 +1,45 @@
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q,Prefetch
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
 # Lista de Clientes: Todos los clientes filtrados por sexo, nombre y fecha de nacimiento.
+def listar_clientes(request):
+    clientes=Cliente.objects.order_by("sexo","nombre","fecha_nacimiento").all()
+    return render(request,"clientes/listar_clientes.html",{'views_listar_cliente':clientes})
 
 # Citas de un Cliente: Todas las citas de un cliente específico.
+def cita_cliente(request,id_cliente):
+    citas=Cita.objects.select_related("cliente","estacion")
+    citas=citas.filter(cliente_id=id_cliente).get()
+    return render(request,"citas/cita_cliente.html",{'views_cita_cliente':citas})
+    
+# Estaciones ITV con Locales: Estaciones ITV junto a su local, ordenadas por el precio del local.
+def estaciones_con_locales(request):
+    estaciones=EstacionItv.objects.select_related("local").order_by("local__precio").all()
+    return render(request,"estaciones/estaciones_con_locales_ordenados.html",{'views_estaciones_con_locales':estaciones})
 
-# Estaciones ITV con Locales: Estaciones ITV junto a su local, ordenadas alfabéticamente.
-
-# Trabajadores de una Estación: Trabajadores de una estación ITV específica.
-
+# Trabajadores de una Estación: todos los datos de los trabajadores de una estación ITV específica.
+def trabajadores_estacion(request,id_estacion):
+    estacion=EstacionItv.objects.get(id=id_estacion)
+    trabajadores=Trabajador.objects.prefetch_related("estacion",
+                                                     Prefetch("trabajador_Inspeccion"),
+                                                     Prefetch("trabajador_Vehiculo"))
+    trabajadores=trabajadores.filter(estacion=id_estacion).all()
+    return render(request,"trabajadores/trabajadores_estacion_especifica.html",{'views_trabajadores_estacion':trabajadores,'estaciones':estacion})
 # Inspecciones de un Vehículo: Inspecciones de un vehículo específico por matrícula.
-
+def inspecciones_vehiculo(request,matricula):
+    inspecciones=Inspeccion.objects.select_related("trabajador","vehiculo")
+    inspecciones=inspecciones.filter(vehiculo__matricula=matricula).all()
+    return render(request,"inspecciones/inspecciones_vehiculo.html",{'views_inspecciones_vehiculo':inspecciones})
 # Detalle de una Maquinaria y Empresa: Detalle de una maquinaria específica y su empresa externa asociada.
 
 # Citas de una Estación en Rango de Fechas: Citas de una estación ITV en un rango de fechas determinado.
 
 # Conteo de Vehículos por Combustible: Número total de vehículos agrupados por tipo de combustible.
 
-# Facturas no Pagadas: Facturas no pagadas, ordenadas por fecha de emisión descendente.
+# Muestra todas las citas de una estación ITV, filtradas por el ID del cliente y el tipo de inspección
 
 # Vehículos sin Trabajadores Asociados: Vehículos que no tienen trabajadores asociados.
